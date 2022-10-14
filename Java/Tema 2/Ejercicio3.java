8e495e0506d467633d26ad30cd8a2acab27a2aca
Audionoob <98852220+Audionoob@users.noreply.github.com>
2022-10-14 18:10:15 +0200

    Add files via upload

---
+++ Java/Tema 2/Ejercicio3.java
@@ -0,0 +1,16 @@
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Introduce las palabras separadas por espacios");
+
+        String palabras = System.console().readLine();
+
+        String[] listapalabras = palabras.split(" ");    
+
+        
+
+
+    }
+}
